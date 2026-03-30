4030dc35e6d936ad120e9e5dd6113f340e32d704
Tiago-Davila <tiagodavila08@gmail.com>
2026-03-30 17:44:27 -0300

    cambie la resolucion, agregue menu de inicio y mejore el rebote

--- arkanoid-davila-tiago/scenes/scripts/pelota.gd
+++ arkanoid-davila-tiago/scenes/scripts/pelota.gd
@@ -1,17 +1,17 @@
 extends RigidBody2D
 var velocidad_constante = 500
 func _ready() -> void:
+	linear_velocity = Vector2(350, -350)
 	gravity_scale = 0
 	linear_damp = 0
 	angular_damp = 0
-	linear_velocity = Vector2(350, -350)
 	add_to_group("pelota")
-	var velocidad_constante = 500
-	
 	var material := PhysicsMaterial.new()
-	material.bounce = 1
+	material.bounce = 0.5
 	material.friction = 0
 	physics_material_override = material
+	
+	
 func _physics_process(delta):
 	if linear_velocity.length() != 0:
 		linear_velocity = linear_velocity.normalized() * velocidad_constante
